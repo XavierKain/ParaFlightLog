a6d12b773fa414a13d5a90b39a4a4bec6e593b36
Xavier Kain <xavierkain.consulting@gmail.com>
2025-11-17 12:23:10 -0300

    Initial Commit

---
+++ ParaFlightLog/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ParaFlightLog
+//
+//  Created by Xavier Kain on 17/11/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ParaFlightLog/ParaFlightLogApp.swift
@@ -0,0 +1,32 @@
+//
+//  ParaFlightLogApp.swift
+//  ParaFlightLog
+//
+//  Created by Xavier Kain on 17/11/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ParaFlightLogApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
